8f9d2e191a7c5a702757d75421c4fcb3fdd3df7b
IriDark <idarkmage228@gmail.com>
2025-01-17 20:38:49 +0200

    fix

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ---
 There are too many things to list, but here are some highlights:
-Removal of furry things (Actually it's no more fluffy)
-Merging with Valoria API
-Merging with QuadLib
-Huge refactoring
+- Removal of furry things (Actually it's no more fluffy)
+- Merging with Valoria API
+- Merging with QuadLib
+- Huge refactoring
--- src/main/java/github/iri/tridot/TridotLibClient.java
+++ src/main/java/github/iri/tridot/TridotLibClient.java
@@ -15,6 +15,7 @@ import net.minecraftforge.fml.event.lifecycle.*;
 import java.awt.*;
 
 public class TridotLibClient{
+    public static LoopedSoundInstance BOSS_MUSIC;
     public static TridotSoundInstance COOLDOWN_SOUND;
     public static TridotSoundInstance DUNGEON_MUSIC_INSTANCE;
     public static final ResourceLocation VANILLA_LOC = new ResourceLocation("textures/gui/bars.png");
@@ -34,33 +35,12 @@ public class TridotLibClient{
 
     public static TridotMod MOD_INSTANCE;
     public static TridotPanorama VANILLA_PANORAMA;
-    public static TridotPanorama FLUFFY_PANORAMA;
-
     public static void setupMenu(){
-        MOD_INSTANCE = new TridotMod(TridotLib.ID, TridotLib.NAME, TridotLib.VERSION).setDev("MaxBogomol").setItem(new ItemStack(Items.PINK_PETALS))
-        .setEdition(TridotLib.VERSION_NUMBER).setNameColor(new Color(254, 200, 207)).setVersionColor(new Color(92, 72, 90))
-        .setDescription(Component.translatable("mod_description.fluffy_fur"))
-        .addGithubLink("https://github.com/MaxBogomol/FluffyFur")
-        .addCurseForgeLink("https://www.curseforge.com/minecraft/mc-mods/fluffy-fur")
-        .addModrinthLink("https://modrinth.com/mod/fluffy-fur")
-        .addDiscordLink("https://discord.gg/cKf55qNugw");
+        MOD_INSTANCE = new TridotMod(TridotLib.ID, TridotLib.NAME, TridotLib.VERSION).setDev("-").setItem(new ItemStack(Items.PINK_PETALS))
+        .setEdition(TridotLib.VERSION_NUMBER).setNameColor(new Color(254, 200, 207)).setVersionColor(new Color(92, 72, 90));
+
         VANILLA_PANORAMA = new TridotPanorama("minecraft:vanilla", Component.translatable("panorama.minecraft.vanilla")).setItem(new ItemStack(Items.GRASS_BLOCK));
-        FLUFFY_PANORAMA = new TridotPanorama(TridotLib.ID + ":fluffy_zone", Component.translatable("panorama.fluffy_fur.fluffy_zone"))
-        .setMod(MOD_INSTANCE).setItem(new ItemStack(Items.CHERRY_LEAVES))
-        .setTexture(new ResourceLocation(TridotLib.ID, "textures/gui/title/background/panorama"))
-        .setLogo(new ResourceLocation(TridotLib.ID, "textures/gui/title/fluffy_fur.png"));
-
-        registerMod(MOD_INSTANCE);
-        registerPanorama(VANILLA_PANORAMA);
-        registerPanorama(FLUFFY_PANORAMA);
-    }
-
-    public static void registerMod(TridotMod mod){
-        TridotModsHandler.registerMod(mod);
-    }
-
-    public static void registerPanorama(TridotPanorama panorama){
-        TridotModsHandler.registerPanorama(panorama);
+        TridotModsHandler.registerPanorama(VANILLA_PANORAMA);
     }
 
     public static void setupSplashes(){
