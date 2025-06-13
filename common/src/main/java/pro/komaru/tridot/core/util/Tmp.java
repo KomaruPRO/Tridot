cf1658a0837fbaaa4636efc858152d77aabc0b34
Skoow <89569841+VuzZis@users.noreply.github.com>
2025-06-13 22:48:01 +0500

    Update Tmp.java

--- common/src/main/java/pro/komaru/tridot/core/util/Tmp.java
+++ common/src/main/java/pro/komaru/tridot/core/util/Tmp.java
@@ -1,6 +1,9 @@
 package pro.komaru.tridot.core.util;
 
 import pro.komaru.tridot.core.math.Rand;
+import pro.komaru.tridot.core.phys.Col;
+import pro.komaru.tridot.core.phys.Vec2;
+import pro.komaru.tridot.core.phys.Vec3;
 
 public class Tmp {
     public static Rand
@@ -8,4 +11,21 @@ public class Tmp {
         rnd2 = Rand.of(1),
         rnd3 = Rand.of(2),
         rnd4 = Rand.of(3);
+    public static Vec2
+        v1 = new Vec2(),
+        v2 = new Vec2(),
+        v3 = new Vec2(),
+        v4 = new Vec2();
+    public static Vec3
+        v31 = new Vec3(),
+        v32 = new Vec3(),
+        v33 = new Vec3(),
+        v34 = new Vec3(),
+        v35 = new Vec3(),
+        v36 = new Vec3();
+    public static Col
+        c1 = new Col(0),
+        c2 = new Col(0),
+        c3 = new Col(0),
+        c4 = new Col(0);
 }
