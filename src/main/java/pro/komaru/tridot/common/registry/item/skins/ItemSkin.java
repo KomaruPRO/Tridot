5ebf551cc7857876124daae033f624449e2e41c8
IriDark <idarkmage228@gmail.com>
2025-08-28 11:29:21 +0300

    test

--- src/main/java/pro/komaru/tridot/common/registry/item/skins/ItemSkin.java
+++ src/main/java/pro/komaru/tridot/common/registry/item/skins/ItemSkin.java
@@ -84,7 +84,7 @@ public class ItemSkin{
 
     @OnlyIn(Dist.CLIENT)
     public HumanoidModel<?> getHumanoidModel(LivingEntity entity, ItemStack stack, EquipmentSlot slot, HumanoidModel<?> _default){
-        return Structs.safeGet(entries.find(e -> e.appliesOn(stack)), e -> e.humanoidModel(entity,stack,slot,_default), () -> getArmorModel(entity, stack, slot, _default));
+        return Structs.safeGet(entries.find(e -> e.appliesOn(stack)), e -> e.humanoidModel(entity, stack, slot, _default), () -> (HumanoidModel<?>) getArmorModel(entity, stack, slot, _default));
     }
 
     @OnlyIn(Dist.CLIENT)
