e2da97e8e59c639b33a3cde9c85f1f2e071f1d2e
IriDark <idarkmage228@gmail.com>
2026-01-15 17:25:14 +0200

    fps drop fix

--- src/main/java/pro/komaru/tridot/mixin/client/ForgeGuiMixin.java
+++ src/main/java/pro/komaru/tridot/mixin/client/ForgeGuiMixin.java
@@ -18,30 +18,28 @@ import static pro.komaru.tridot.common.Events.GUI_ICONS_LOCATION;
 @Mixin(ForgeGui.class)
 public class ForgeGuiMixin{
 
+    @Unique private float tridot$lastArmorValue = -1.0F;
+    @Unique private String tridot$cachedArmorText = "";
+
     @Inject(method = "renderArmor", at = @At("HEAD"), cancellable = true, remap = false)
     protected void renderArmor(GuiGraphics guiGraphics, int width, int height, CallbackInfo ci) {
-        tridot$self().getMinecraft().getProfiler().push("armor");
-        RenderSystem.enableBlend();
-        int left = width / 2 - 91;
-        int top = height - tridot$self().leftHeight;
-        float armor = (float)tridot$self().getMinecraft().player.getAttributeValue(AttributeRegistry.PERCENT_ARMOR.get());
-        if(armor > 0 && CommonConfig.PERCENT_ARMOR.get()){
-            RenderSystem.enableBlend();
-            RenderSystem.defaultBlendFunc();
-            RenderSystem.disableDepthTest();
-            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-            RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        float currentArmor = (float) tridot$self().getMinecraft().player.getAttributeValue(AttributeRegistry.PERCENT_ARMOR.get());
+        if (currentArmor > 0 && CommonConfig.PERCENT_ARMOR.get()) {
+            int left = width / 2 - 91;
+            int top = height - tridot$self().leftHeight;
 
-            RenderSystem.disableBlend();
-            String component = I18n.get("tooltip.tridot.value", String.format("%.1f%%", armor));
             guiGraphics.blit(GUI_ICONS_LOCATION, left, top - 1, 34, 9, 9, 9);
-            guiGraphics.drawString(tridot$self().getMinecraft().font, component, left + 10, top, Color.WHITE.getRGB());
+            if (Math.abs(currentArmor - tridot$lastArmorValue) > 0.01F) {
+                String formattedValue = String.format("%.1f%%", currentArmor);
+                tridot$cachedArmorText = I18n.get("tooltip.tridot.value", formattedValue);
+                tridot$lastArmorValue = currentArmor;
+            }
+
+            guiGraphics.drawString(tridot$self().getMinecraft().font, tridot$cachedArmorText, left + 10, top, 0xFFFFFF);
+
             tridot$self().leftHeight += 10;
-            RenderSystem.disableBlend();
             ci.cancel();
         }
-
-        tridot$self().getMinecraft().getProfiler().pop();
     }
 
     @Unique
