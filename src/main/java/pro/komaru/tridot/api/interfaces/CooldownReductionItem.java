b6d3db8a785267c937c0ac88a25170af98fdaf84
IriDark <idarkmage228@gmail.com>
2026-04-28 16:10:17 +0300

    cooldown reduction fix

--- src/main/java/pro/komaru/tridot/api/interfaces/CooldownReductionItem.java
+++ src/main/java/pro/komaru/tridot/api/interfaces/CooldownReductionItem.java
@@ -6,7 +6,14 @@ import pro.komaru.tridot.common.registry.EnchantmentsRegistry;
 public interface CooldownReductionItem{
     default int getCooldownReduction(int cooldown, ItemStack stack) {
         int level = stack.getEnchantmentLevel(EnchantmentsRegistry.OVERDRIVE.get());
-        float modifier = 0.15f + ((level - 1) * 0.05f);
+        float modifier = 1.0f;
+        if (level > 0) {
+            modifier = 0.90f;
+            if (level > 1) {
+                modifier -= (level - 1) * 0.05f;
+            }
+        }
+
         return Math.max(0, Math.round(cooldown * modifier));
     }
 }
